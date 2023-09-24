1c4bb3351fc4562622ad35052cd3ea914f67e1e0
Vikas <vikasmaur10@gmail.com>
2023-09-24 16:28:41 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Navbar.jsx';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="w-24 h-24 rounded-full bg-red-500"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,28 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a className="underline text-blue-500 hover:text-blue-300" href="/">
+            Home
+          </a>
+        </li>
+        <li>
+          <a className="underline text-blue-500 hover:text-blue-300" href="/">
+            Why DDC
+          </a>
+        </li>
+        <li>
+          <a className="underline text-blue-500 hover:text-blue-300" href="/">
+            Achievements
+          </a>
+        </li>
+        <li>
+          <a className="underline text-blue-500 hover:text-blue-300" href="/">
+            Event
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
